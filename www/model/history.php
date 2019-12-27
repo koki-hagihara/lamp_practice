b4d9c202d81d4e8273c63d77b97c0f289282ccda
koki hagihara <qqgy9xn9k@leaf.ocn.ne.jp>
2019-12-27 23:32:47 +0900

    test

--- www/model/history.php
+++ www/model/history.php
@@ -3,5 +3,8 @@ require_once 'functions.php';
 require_once 'db.php';
 
 function record_order_history() {
-    
+    $sql = "INSERT INTO
+            order_history (
+            user_id,
+            "
 }
