aafc9a71f24e049ed48b85267ab7b022eb4074ac
koki hagihara <qqgy9xn9k@leaf.ocn.ne.jp>
2020-01-05 16:26:15 +0900

    ランキング機能追加

--- www/html/index.php
+++ www/html/index.php
@@ -7,6 +7,7 @@ require_once '../model/functions.php';
 require_once '../model/user.php';
 //商品情報登録に関する関数ファイル読み込み
 require_once '../model/item.php';
+require_once '../model/ranking.php';
 
 //セッション開始(ログインチェック)
 session_start();
@@ -24,6 +25,10 @@ $user = get_login_user($db);
 
 //公開ステータスになっている商品全てのデータを取得
 $items = get_open_items($db);
+
+$ranking = get_ranking($db);
+
+$rank = 1;
 
 //ビューファイル読み込み
 include_once '../view/index_view.php';
--- www/view/index_view.php
+++ www/view/index_view.php
@@ -14,10 +14,14 @@
     <h1>商品一覧</h1>
     <?php include VIEW_PATH . 'templates/messages.php'; ?>
 
+<!--card-deck:隣り合うカード型デザインの高さを自動的に揃えてくれる -->
     <div class="card-deck">
+    <!--row:行に与えるクラス-->
       <div class="row">
       <?php foreach($items as $item){ ?>
+      <!--col:列を表すクラス-->
         <div class="col-6 item">
+        <!--card:画像やタイトル等をくくる、h-100:height100%、text-center:テキスト中央寄せ-->
           <div class="card h-100 text-center">
             <div class="card-header">
               <?php print(entity_str($item['name'])); ?>
@@ -40,6 +44,36 @@
         </div>
       <?php } ?>
       </div>
+    </div>
+    <h1>人気ランキング</h1>
+    <div class="card-deck">
+      <div class="row">
+      <?php foreach ($ranking as $value) { ?>
+        <div class="col-4 item">  
+          <div class="card h-100 text-center">
+            <div class="card-header">
+              <?php print $rank;?>位
+              <?php $rank++;?>
+            </div>
+            <figure class="card-body">
+              <img class="card-img" src="<?php print(IMAGE_PATH . entity_str($value['image'])); ?>">
+              <figcaption>
+                <div class="card-title"><?php print(entity_str($value['name'])); ?></div>
+                <div class="card-text"><?php print(number_format(entity_str($value['price']))); ?>円</div>
+                <?php if($value['stock'] > 0){ ?>
+                  <form action="index_add_cart.php" method="post">
+                    <input type="submit" value="カートに追加" class="btn btn-primary btn-block">
+                    <input type="hidden" name="item_id" value="<?php print($value['item_id']); ?>">
+                  </form>
+                <?php } else { ?>
+                  <p class="text-danger">現在売り切れです。</p>
+                <?php } ?>
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+      <?php } ?>
+      </div>           
     </div>
   </div>
   
